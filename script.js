5720919c69552bf7c45506cf7a9a82960df05cf1
Ash Davey <123456789+adavey008@googlemail.com>
2024-09-09 19:59:26 +0100

    You can now draw, previously only one pixel would colour in however it now works, had to cave in after a couple hours of looking for the answer, got close but not close enough

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 // Creating Div's for the screen
-for (x = 0; x < 889; x++) {
+for (x = 0; x < 888; x++) {
     var pixel = document.createElement("div");
     pixel.className = "pixel";
-
+    pixel.id = "pixel";
     document.getElementById("screen").appendChild(pixel);
 }
 
 
 // Change colour when hover over with mouse
-const draw = document.querySelector(".pixel");
-
-draw.addEventListener("mouseover", () => {
-    draw.style.backgroundColor = "green";
+document.querySelectorAll(".pixel").forEach((pixel) => {
+    pixel.addEventListener("mouseover", ()=> {
+        pixel.style.background = "green";
+    })
 });
+
+
 
