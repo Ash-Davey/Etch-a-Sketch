0506aee2714655f10ed6feb5fd0c3493a749b778
Ash Davey <123456789+adavey008@googlemail.com>
2024-09-10 20:23:51 +0100

    This now has rbg colours and will slowly fill in to a full colour after 10 times, i still need to add a grid that changes in size with value input.

--- script.js
+++ script.js
@@ -2,15 +2,24 @@
 for (x = 0; x < 888; x++) {
     var pixel = document.createElement("div");
     pixel.className = "pixel";
-    pixel.id = "pixel";
     document.getElementById("screen").appendChild(pixel);
 }
+
+
 
 
 // Change colour when hover over with mouse
 document.querySelectorAll(".pixel").forEach((pixel) => {
     pixel.addEventListener("mouseover", ()=> {
-        pixel.style.background = "green";
+        //Random colour
+        const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+        const r = randomBetween (0, 255);
+        const g = randomBetween (0, 255);
+        const b = randomBetween (0, 255);
+        const rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+        pixel.style.background = rgb;
+        pixel.style.opacity = (parseFloat(pixel.style.opacity) || 0) + 0.1;
+
     })
 });
 
