4ae760443d8ab343b30904beb0ae3f9a39fbad67
Ash Davey <123456789+adavey008@googlemail.com>
2024-09-18 22:38:39 +0100

    Ui finished, js completed with the extra tasks. Ability to change grid size and clear, pixels aren't square due to design.

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
+//Default value for grid when first loaded
+var defaultTable = 10;
+createTable();
+
+//Update grid when clicking button
+document.getElementById("left").addEventListener("click", () => {
+    var userInput = prompt("Enter a number below 100 to change grid size.");
+
+    if (userInput > 100) {
+        alert("Choose a number below 100!");
+    } else {
+        defaultTable = userInput;
+    }
+    createTable();
+});
+
+//Clear screen
+document.getElementById("right").addEventListener("click", () => {
+    document.getElementById("screen").textContent = "";
+    createTable();
+});
+
 // Creating Div's for the screen
-for (x = 0; x < 888; x++) {
-    var pixel = document.createElement("div");
-    pixel.className = "pixel";
-    document.getElementById("screen").appendChild(pixel);
-}
+function createTable() {
+    document.getElementById("screen").textContent = "";
 
+    percent = 100 / defaultTable;
 
+    for (x = 0; x < defaultTable ** 2; x++) {
 
-
-// Change colour when hover over with mouse
-document.querySelectorAll(".pixel").forEach((pixel) => {
+        var pixel = document.createElement("div");
+        pixel.className = "pixel";
+        pixel.style.height = percent + "%";
+        pixel.style.width = percent + "%";
+        document.getElementById("screen").appendChild(pixel);
+    }
+    // Change colour when hover over with mouse
+    document.querySelectorAll(".pixel").forEach((pixel) => {
     pixel.addEventListener("mouseover", ()=> {
         //Random colour
         const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
@@ -22,6 +48,9 @@ document.querySelectorAll(".pixel").forEach((pixel) => {
 
     })
 });
+}
+
+
 
 
 
